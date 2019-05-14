9adffdb84819a69202d78f07fe191bbd24442979
csmark97 <36475822+csmark97@users.noreply.github.com>
2019-05-14 04:36:18 +0200

    bugfixes on fileupload

--- Helper/FileHelpers.cs
+++ Helper/FileHelpers.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CarService.Dal.Entities;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
@@ -14,13 +15,13 @@ namespace CarService.Bll.Helper
 {
     public class FileHelpers
     {
-        public static async Task UploadAsync(string path, FileUpload fileUpload)
+        public static async Task UploadAsync(string path, IFormFile fileUpload)
         {
-            var filePath = path + fileUpload.Upload.FileName;
+            var filePath = path + fileUpload.FileName;
 
             using (var fileStream = new FileStream(filePath, FileMode.Create))
             {
-                await fileUpload.Upload.CopyToAsync(fileStream);
+                await fileUpload.CopyToAsync(fileStream);
             }
         }
     }
